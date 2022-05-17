d29597fd00ae585e7bbea5fa4b010d0ff22f513f
Conor Barnes <conorbarnes93@gmail.com>
2022-05-17 16:26:00 -0300

    compact cards

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,37 +1,47 @@
 import { Billionaire } from "../lib/data";
 import { motion } from "framer-motion";
-export default function Card({ billi }: { billi: Billionaire }) {
+export default function Card({ billi, compact }: { billi: Billionaire; compact: boolean }) {
 	const { grade } = billi;
 
 	return (
 		<motion.div
 			layout
-			className="bg-white border-black shadow-red-800 shadow-md w-[500px] max-w-full p-4 m-3"
+			animate={{ height: compact ? 72 : 320 }}
+			transition={{ type: "tween" }}
+			className={`bg-white border-black shadow-red-800 shadow-md w-[500px] max-w-full p-4 m-3 overflow-none}`}
 		>
 			<div className="flex">
-				<div className="flex-1">
+				<div className="flex-1 h-[72px]">
 					<h1 className="text-2xl">{billi.name}</h1>
 					<p className="text-xs italic">${billi.wealth.toLocaleString()}</p>
 				</div>
-				<div className="w-16">
-					<p
-						className={`font-mono font-bold text-7xl ${
-							grade === "A"
-								? "text-green-400"
-								: grade === "B"
-								? "text-blue-500"
-								: grade === "C"
-								? "text-yellow-400"
-								: grade === "D"
-								? "text-orange-500"
-								: "text-red-500"
-						}`}
-					>
-						{billi.grade}
-					</p>
-				</div>
+				{/* <div className="h-full border-2"> */}
+				<p
+					className={`font-mono font-bold text-7xl h-0 transition-transform duration-[400ms] ${
+						compact ? "scale-50 translate-y-2" : "scale-100 -translate-x-4"
+					} ${
+						grade === "A"
+							? "text-green-400"
+							: grade === "B"
+							? "text-blue-500"
+							: grade === "C"
+							? "text-yellow-400"
+							: grade === "D"
+							? "text-orange-500"
+							: "text-red-500"
+					}`}
+				>
+					{billi.grade}
+				</p>
+				{/* </div> */}
 			</div>
-			<div className="mt-2">{billi.bio}</div>
+			<div
+				className={`mt-2 ${
+					compact && "scale-y-0 duration-200"
+				} transition-transform origin-top duration-[400ms]`}
+			>
+				{billi.bio}
+			</div>
 		</motion.div>
 	);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,13 @@ import { useState } from "react";
 import Sorter from "../components/Sorter";
 import Card from "../components/Card";
 import { Billionaire, billionaires } from "../lib/data";
+import * as Checkbox from "@radix-ui/react-checkbox";
 
 export default function Home() {
 	const [order, setOrder] = useState<"wealth:high" | "wealth:low" | "impact:high" | "impact:low">(
 		"wealth:high"
 	);
+	const [compact, setCompact] = useState(false);
 
 	let sortedBillionaires: Billionaire[] = [];
 	switch (order) {
@@ -36,7 +38,7 @@ export default function Home() {
 				openGraph={{ title: "CNR APP", description: "CNR APP description" }}
 			/>
 
-			<main className="py-20 px-4">
+			<main className="py-20 px-4 flex flex-col">
 				<article className="prose mb-12">
 					<h1 className="text-center ">Billionaire Impact Ranking</h1>
 					<p className="text-center">
@@ -48,39 +50,66 @@ export default function Home() {
 
 					<p className="text-center">
 						This site grades billionaires by the positive impact of their giving. If you
-						would like to learn more about making an impact with your money you might
-						be an{" "}
+						would like to learn more about making an impact with your money you might be
+						an{" "}
 						<a href="https://www.effectivealtruism.org/articles/introduction-to-effective-altruism">
 							effective altruist
 						</a>
 						!
 					</p>
 				</article>
-				<div className="flex justify-center">
-					<Sorter
-						selected={
-							order === "wealth:high" ? "down" : order === "wealth:low" ? "up" : "naw"
-						}
-						onClick={() =>
-							setOrder(order === "wealth:high" ? "wealth:low" : "wealth:high")
-						}
-					>
-						Wealth
-					</Sorter>
-					<Sorter
-						selected={
-							order === "impact:high" ? "down" : order === "impact:low" ? "up" : "naw"
-						}
-						onClick={() =>
-							setOrder(order === "impact:high" ? "impact:low" : "impact:high")
-						}
-					>
-						Impact
-					</Sorter>
+				<div className="flex justify-between w-3/4 items-center self-center">
+					<div className="flex justify-center items-center">
+						<Sorter
+							selected={
+								order === "wealth:high"
+									? "down"
+									: order === "wealth:low"
+									? "up"
+									: "naw"
+							}
+							onClick={() =>
+								setOrder(order === "wealth:high" ? "wealth:low" : "wealth:high")
+							}
+						>
+							Wealth
+						</Sorter>
+						<Sorter
+							selected={
+								order === "impact:high"
+									? "down"
+									: order === "impact:low"
+									? "up"
+									: "naw"
+							}
+							onClick={() =>
+								setOrder(order === "impact:high" ? "impact:low" : "impact:high")
+							}
+						>
+							Impact
+						</Sorter>
+					</div>
+					<label className="font-bold cursor-pointer flex items-center group">
+						Compact
+						<Checkbox.Root
+							checked={compact}
+							onCheckedChange={() => setCompact((c) => !c)}
+						>
+							<div className="border-2 border-black rounded-md w-4 h-4 flex items-center justify-center ml-2">
+								<span
+									className={`${
+										compact ? "opacity-100" : "opacity-0 group-hover:opacity-50"
+									} transition-opacity text-3xl relative left-0.5 -top-0.5`}
+								>
+									✓
+								</span>
+							</div>
+						</Checkbox.Root>
+					</label>
 				</div>
 				<div className="flex flex-col items-center">
 					{sortedBillionaires.map((b) => (
-						<Card billi={b} key={b.name} />
+						<Card billi={b} key={b.name} compact={compact} />
 					))}
 				</div>
 			</main>

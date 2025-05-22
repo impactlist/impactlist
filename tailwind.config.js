915ea37a8fac04230d99ca23b360843f1ea9e00a
Elliot Olds <5849793+elliotolds@users.noreply.github.com>
2025-05-22 18:40:40 +0200

    make h1 less prominent

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,20 @@ import typography from '@tailwindcss/typography';
 export default {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            h1: {
+              fontSize: '1.5em',
+              fontWeight: '700',
+              marginTop: '1.333em',
+              marginBottom: '0.666em',
+            },
+          },
+        },
+      },
+    },
   },
   plugins: [typography],
 };

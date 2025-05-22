bf4a84d32a37afb10508a4104691d4fbcd7b6f9e
Elliot Olds <5849793+elliotolds@users.noreply.github.com>
2025-05-22 12:13:19 +0200

    factor out assumptions button into its own component

--- src/components/DonationCalculator.jsx
+++ src/components/DonationCalculator.jsx
@@ -12,6 +12,7 @@ import { useCostPerLife } from './CostPerLifeContext';
 import CustomValuesIndicator from './CustomValuesIndicator';
 import SpecificDonationModal from './SpecificDonationModal';
 import PageHeader from './PageHeader';
+import AdjustAssumptionsButton from './AdjustAssumptionsButton';
 
 // Import new components
 import CalculatorStats from './calculator/CalculatorStats';
@@ -356,24 +357,7 @@ const DonationCalculator = () => {
           <div className="flex justify-end items-center mb-6">
             <div className="flex items-center space-x-3">
               <CustomValuesIndicator />
-              <button
-                onClick={openModal}
-                className="inline-flex items-center px-3 py-1.5 border border-indigo-600 text-indigo-600 bg-white rounded-md text-sm font-medium hover:bg-indigo-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-4 w-4 mr-1.5"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-                Adjust Assumptions
-              </button>
+              <AdjustAssumptionsButton onClick={openModal} />
             </div>
           </div>
 

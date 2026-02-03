882ae83daff81ff37df9560afbd67654716e9c9f
Elliot Olds <5849793+elliotolds@users.noreply.github.com>
2026-02-03 01:43:04 +0100

    back to top donors should be the text of every page's back button

--- src/pages/AssumptionsPage.jsx
+++ src/pages/AssumptionsPage.jsx
@@ -65,7 +65,7 @@ const AssumptionsPage = () => {
       exit={{ opacity: 0 }}
       transition={{ duration: 0.3 }}
     >
-      <BackButton to="/" />
+      <BackButton to="/" label="Back to top donors" />
 
       <motion.div
         className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pb-8"
